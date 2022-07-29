1a8f0caf5b79c6c713f67016e3cd82d974be8d77
Naughty1029 <masahiro.watanabe02@gmail.com>
2022-07-29 12:17:39 +0900

    他のタグも存在するかチェック

--- rtl_study/src/Render.test.js
+++ rtl_study/src/Render.test.js
@@ -6,11 +6,14 @@ describe("Rendering",()=>{
   it("Should render all the elments correctly",()=>{
     render(<Render />);//renderコンポーネントを取得
     //screen.debug();//取得したコンポーネントをhtmlとして描画
-    
+
     // https://github.com/A11yance/aria-query#elements-to-roles
     // screen.debug(screen.getByRole("heading"));//headingのロールを持つ要素を描画
 
     //https://jestjs.io/ja/docs/expect
-    expect(screen.getAllByRole('heading')).toBeTruthy();//存在するかどうかのチェック
+    expect(screen.getByRole('heading')).toBeTruthy();//hタグが存在するかどうかのチェック
+    expect(screen.getByRole('textbox')).toBeTruthy();//inputタグが存在するかのチェック
+    expect(screen.getAllByRole("button")[0]).toBeTruthy();//buttonが存在するかのチェック
+    expect(screen.getAllByRole("button")[1]).toBeTruthy();//buttonが存在するかのチェック
   })
 })
