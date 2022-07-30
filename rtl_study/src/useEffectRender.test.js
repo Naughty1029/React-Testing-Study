6327e1e221b92baf18045799f21c4b56134a04be
渡邉雅広 <watanabemasahiro@masahiro.local>
2022-07-30 16:51:51 +0900

    Mock Server

--- rtl_study/src/useEffectRender.test.js
+++ rtl_study/src/useEffectRender.test.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { render,screen } from "@testing-library/react";
-import { EffectRender } from "./useEffectRender";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { EffectRender } from './useEffectRender';
 
-describe("useEffect rendering",()=>{
-    it("Should render only after async function resolved", async ()=>{
-        render(<EffectRender />)
-        expect(screen.queryByText(/I am/)).toBeNull()
-        expect(await screen.findByText(/I am/)).toBeInTheDocument()
-    })
-})
+describe('useEffect rendering', () => {
+  it('Should render only after async function resolved', async () => {
+    render(<EffectRender />);
+    expect(screen.queryByText(/I am/)).toBeNull();
+    expect(await screen.findByText(/I am/)).toBeInTheDocument();
+  });
+});
