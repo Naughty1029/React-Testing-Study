db0b491b2657c5dd427f73cac76b68850cfcb517
Naughty1029 <masahiro.watanabe02@gmail.com>
2022-07-30 09:22:14 +0900

    UserEvent

---
+++ rtl_study/src/RenderInput.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+const RenderInput = ({outputConsole}) => {
+  const [input,setInput] = useState("");
+  const outputValue = ()=> {
+    if(input){
+      outputConsole(input)
+    }
+  }
+
+  const upadateValue = (e)=> {
+    setInput(e.target.value);
+  }
+
+  return (
+    <div>
+      <input type="text" placeholder='Enter' value={input} onChange={upadateValue} />
+      <button onClick={outputValue}>Console</button>
+    </div>
+  )
+}
+
+export default RenderInput
